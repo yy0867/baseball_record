51f1f48e4256d35bd14e6b71cb17ea100fd4329a
yy0867 <yy0867@naver.com>
2021-01-03 16:27:16 +0900

    Complete Menu Activity, Add 'Total, Add, Each, Manage' Activities

--- app/src/main/java/com/harry/yaguban/MainActivity.java
+++ app/src/main/java/com/harry/yaguban/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         startActivity(new Intent(this, LoadingActivity.class));
+    }
 
+    public void buttonTotalClicked(View v) {
+        startActivity(new Intent(this, TotalActivity.class));
+    }
+
+    public void buttonAddClicked(View v) {
+        startActivity(new Intent(this, AddActivity.class));
+    }
+
+    public void buttonEachClicked(View v) {
+        startActivity(new Intent(this, EachActivity.class));
+    }
+
+    public void buttonManageClicked(View v) {
+        startActivity(new Intent(this, ManageActivity.class));
     }
 }
