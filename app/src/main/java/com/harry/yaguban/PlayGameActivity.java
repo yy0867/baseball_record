7c01ebf5bf1bbb92c6a55f3faa79218f57bd4c90
yy0867 <yy0867@naver.com>
2021-01-08 23:07:26 +0900

    add @drawable/out_k.png, in PlayGameActivity->out complete

--- app/src/main/java/com/harry/yaguban/PlayGameActivity.java
+++ app/src/main/java/com/harry/yaguban/PlayGameActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,7 +17,9 @@ public class PlayGameActivity extends AppCompatActivity {
     Game newGame;
     boolean isDefense;
     TextView textBatPitch;
+    ImageView outCounts;
     SwitchCompat switchBatPitch;
+    Typeface font, boldfont;
     TableRow rowInnings, rowHomeTeam, rowAwayTeam;
 
     @Override
@@ -28,8 +32,11 @@ public class PlayGameActivity extends AppCompatActivity {
         TextView opponentTeamName = findViewById(R.id.textviewVS);
         switchBatPitch = findViewById(R.id.switchBatPitch);
         textBatPitch = findViewById(R.id.textviewBatPitch);
+        outCounts = findViewById(R.id.imageviewOuts);
 
         newGame = (Game)intent.getSerializableExtra("objectGame");
+        font = Typeface.createFromAsset(getAssets(), "font/bccardlight.ttf");
+        boldfont = Typeface.createFromAsset(getAssets(), "font/bccardbold.ttf");
 
         //first defense -> first pitcher
         isDefense = newGame.getHomeTeam().equals(Game.ourTeam);
@@ -44,7 +51,7 @@ public class PlayGameActivity extends AppCompatActivity {
         rowInnings = findViewById(R.id.tablerowInning);
         rowHomeTeam = findViewById(R.id.tablerowHomeTeam);
         rowAwayTeam = findViewById(R.id.tablerowAwayTeam);
-        setRowScoreBoard();
+        setScoreBoards();
     }
 
     private void changeBatPitchText() {
@@ -55,19 +62,61 @@ public class PlayGameActivity extends AppCompatActivity {
         }
     }
 
-    private void setRowScoreBoard() {
-        Typeface font = Typeface.createFromAsset(getAssets(), "font/bccardlight.ttf");
-        //set row Innings
-        rowInnings.setWeightSum(9);
-        for (int i = 0; i < Game.inning; i++) {
-            TextView tableText = new TextView(this);
+    private void setScoreBoards() {
+        setRowInnings();
+        setRowScore(rowAwayTeam, Game.away);
+        setRowScore(rowHomeTeam, Game.home);
 
-            tableText.setText(String.valueOf(i + 1));
-            tableText.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1));
-            tableText.setTypeface(font);
-            tableText.setTextSize(20);
-            rowInnings.addView(tableText);
+        TableLayout.LayoutParams rowParams = (TableLayout.LayoutParams) rowInnings.getLayoutParams();
+        rowParams.bottomMargin = 20;
+
+        rowInnings.setLayoutParams(rowParams);
+        rowAwayTeam.setLayoutParams(rowParams);
+        rowHomeTeam.setLayoutParams(rowParams);
+    }
+
+    //set row Innings
+    private void setRowInnings() {
+        rowInnings.setWeightSum(9);
+        rowInnings.addView(setTableText("이닝", 1.8f, true));
+
+        for (int i = 0; i < Game.inning; i++) {
+            rowInnings.addView(setTableText(Integer.toString(i + 1), 0.8f, true));
         }
+    }
+
+    private void reloadScoreBoard() {
+        rowAwayTeam.removeAllViews();
+        rowHomeTeam.removeAllViews();
+        setRowScore(rowAwayTeam, Game.away);
+        setRowScore(rowHomeTeam, Game.home);
+    }
+
+    private void setRowScore(TableRow rowWhichTeam, boolean isHomeTeam) {
+        String teamName;
+
+        if(isHomeTeam) teamName = newGame.getHomeTeam().replaceAll("대학교", "");
+        else teamName = newGame.getAwayTeam().replaceAll("대학교", "");;
+
+        rowWhichTeam.setBackgroundColor(getResources().getColor(R.color.background_color));
+        rowWhichTeam.addView(setTableText(teamName, 1.8f, false));
+
+        for (int i = 0; i < Game.inning; i++) {
+            rowWhichTeam.addView(setTableText(Integer.toString(newGame.getTeamScore(isHomeTeam, i)), 0.8f, false));
+        }
+    }
+
+    private TextView setTableText(String value, float weight, boolean bold) {
+        TextView text = new TextView(this);
+        text.setText(value);
+        text.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, weight));
+
+        if (bold) text.setTypeface(boldfont);
+        else text.setTypeface(font);
+
+        text.setTextSize(20);
+
+        return text;
     }
 
     //안타
@@ -87,6 +136,13 @@ public class PlayGameActivity extends AppCompatActivity {
 
     //홈런
     public void homerunClicked(View v) {
+        if(newGame.getCurAttackTeam() == Game.away) {
+            newGame.setScoreAway();
+        } else {
+            newGame.setScoreHome();
+        }
+        reloadScoreBoard();
+
         Toast.makeText(this, "홈런!", Toast.LENGTH_SHORT).show();
     }
 
@@ -107,6 +163,17 @@ public class PlayGameActivity extends AppCompatActivity {
 
     //아웃
     public void outClicked(View v) {
+        newGame.increaseOut();
+
+        if(newGame.getCurOut() == 0)
+            outCounts.setImageResource(R.drawable.out_0);
+        else if(newGame.getCurOut() == 1)
+            outCounts.setImageResource(R.drawable.out_1);
+        else if(newGame.getCurOut() == 2)
+            outCounts.setImageResource(R.drawable.out_2);
+        else if(newGame.getCurOut() == 3)
+            outCounts.setImageResource(R.drawable.out_3);
+
         Toast.makeText(this, "아웃!", Toast.LENGTH_SHORT).show();
     }
 
