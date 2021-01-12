d7e132e16a52ba4d7b2078ab868531606802a3dd
yy0867 <yy0867@naver.com>
2021-01-12 15:45:05 +0900

    Add AddPlayerPopup - Buttons, frame divide by Batter, Pitcher

--- app/src/main/java/com/harry/yaguban/AddPlayerPopupActivity.java
+++ app/src/main/java/com/harry/yaguban/AddPlayerPopupActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
@@ -13,6 +17,12 @@ public class AddPlayerPopupActivity extends AppCompatActivity {
     RadioGroup selectPosition;
     ArrayList<Person> batterInfo;
     ArrayList<Person> pitcherInfo;
+    ArrayAdapter<String> infoAdapter;
+    ArrayList<String> batterString, pitcherString;
+
+    Spinner batterListSpinner, battingOrderSpinner;
+    Spinner pitcherListSpinner;
+    LinearLayout batterListLayout, pitcherListLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +33,51 @@ public class AddPlayerPopupActivity extends AppCompatActivity {
         selectPosition = findViewById(R.id.radiogroupSelectBatPitch);
         batterInfo = (ArrayList<Person>) intent.getSerializableExtra("batterList");
         pitcherInfo = (ArrayList<Person>) intent.getSerializableExtra("pitcherList");
+        batterListSpinner = findViewById(R.id.spinnerSelectBatter);
+        battingOrderSpinner = findViewById(R.id.spinnerSelectBattingOrder);
+        pitcherListSpinner = findViewById(R.id.spinnerSelectPitcher);
+        batterListLayout = findViewById(R.id.layoutBatterList);
+        pitcherListLayout = findViewById(R.id.layoutPitcherList);
+
+        convertPersonListToString();
+        setAdapters();
 
         selectPosition.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                //should make xml list, set visible & invisible
                 if (checkedId == R.id.radiobuttonBatter) {
                     //show batter information list
+                    pitcherListLayout.setVisibility(View.INVISIBLE);
+                    batterListLayout.setVisibility(View.VISIBLE);
                 } else if (checkedId == R.id.radiobuttonPitcher) {
                     //show pitcher information list
+                    batterListLayout.setVisibility(View.INVISIBLE);
+                    pitcherListLayout.setVisibility(View.VISIBLE);
                 }
             }
         });
+    }
+
+    private void setAdapters() {
+        infoAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, batterString);
+        batterListSpinner.setAdapter(infoAdapter);
+
+        infoAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, pitcherString);
+        pitcherListSpinner.setAdapter(infoAdapter);
+    }
+
+    private void convertPersonListToString() {
+        //batter
+        batterString = new ArrayList<>();
+        pitcherString = new ArrayList<>();
+
+        for (Person p : batterInfo) {
+            batterString.add(p.getName() + "  (" + p.getBackNum() + "번)   " + "[" + p.getPosition() + "]");
+        }
+
+        //pitcher
+        for (Person p : pitcherInfo) {
+            pitcherString.add(p.getName() + "  (" + p.getBackNum() + "번)   " + "[" + p.getPosition() + "]");
+        }
     }
 }
